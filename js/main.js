46ba9325c2875c531c9be7cb68e420c2b9506c33
ZeeshanDeveloper <54203438+ZeeshanDeveloper@users.noreply.github.com>
2021-03-21 14:38:56 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
 //main
 //main
 /*mbl-nav*/
-$(".nav-opener").click(function () {
-    $(".mbl-nav").addClass("mbl-nav-opened");
-    $("body").addClass("lock");
-    $(".mbl-nav-opened").bind("click", function () {
-        $(".mbl-nav").removeClass("mbl-nav-opened");
-        $("body").removeClass("lock");
-    });
-});
 $(".mbl-nav .box").click(function (e) {
     e.stopPropagation();
 });
-$(".mbl-dot-nav-opener").click(function () {
+$(".mbl-nav-opener").click(function () {
     $(".mbl-nav").addClass("mbl-nav-opened");
     $("body").addClass("lock");
     $(".mbl-nav-opened").bind("click", function () {
